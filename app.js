243964650849d688403716515de0b32fb7ff8e10
Afif Zuhair <afifzuhair08@gmail.com>
2021-06-29 00:51:37 +0800

    Insert, Update, Delete, Connect with other model, update with other model

--- app.js
+++ app.js
@@ -2,55 +2,92 @@
 const mongoose = require("mongoose");
 
 // initiate new connection from mongoose
-mongoose.connect("mongodb://localhost:27017/fruitsDB", { useNewUrlParser: true , useUnifiedTopology: true});
-
-// declaring the schema for the object "fruits"
-const fruitsSchema = new mongoose.Schema({
-  name: String,
-  rating: Number,
-  review: String
+mongoose.connect("mongodb://localhost:27017/fruitsDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 });
 
+// ----------------------------------------------------------------------------------------------------------
+// declaring the schema for the object "fruits"
+const fruitsSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Name required."]
+  },
+  rating: {
+    type: Number,
+    min: 1,
+    max: 10
+  }, // type and validation
+  review: String
+});
 // initiating model
 const Fruit = mongoose.model("Fruit", fruitsSchema);
-
 // inserting new documents
 const fruit = new Fruit({
   name: "Apple",
   rating: 7,
   review: "Nice taste"
 });
-
 // save the documents
 // fruit.save();
 
+
+// ----------------------------------------------------------------------------------------------------------
 // PERSON
 const personSchema = new mongoose.Schema({
   name: String,
-  age: Number
+  age: Number,
+  favFruits: fruitsSchema //connection to fruits schema
 });
+
 const Person = mongoose.model("Person", personSchema);
+
+// NEW FRUITS
+const mango = new Fruit({
+  name: "Mango",
+  rating: 9,
+  review: "Nice sweet taste"
+});
+// mango.save();
+
+// INSERT NEW PERSON
 const person = new Person({
-  name: "Ali",
-  age: 28
+  name: "Julia",
+  age: 28,
+  favFruits: mango
 });
 // person.save();
 
-const orange = new Fruit({
-  name: "Orange",
-  rating: 8,
-  review: "Nice taste"
+
+// ----------------------------------------------------------------------------------------------------------
+// UPDATE WITH FRUITS SCHEMA
+Person.updateOne(
+  {name: "Ali"},
+  {favFruits: mango},
+  function(err){
+    if(err){
+      console.log(err);
+    }else{
+      console.log("Successfully updated person favFruits!");
+    }
 });
-const lemon = new Fruit({
-  name: "Lemon",
-  rating: 7,
-  review: "Nice taste"
-});
-const pear = new Fruit({
-  name: "Pear",
-  rating: 5,
-  review: "Nice taste"
-});
+
+// const orange = new Fruit({
+//   name: "Orange",
+//   rating: 8,
+//   review: "Nice taste"
+// });
+// const lemon = new Fruit({
+//   name: "Lemon",
+//   rating: 7,
+//   review: "Nice taste"
+// });
+// const pear = new Fruit({
+//   name: "Pear",
+//   rating: 5,
+//   review: "Nice taste"
+// });
 
 // Fruit.insertMany([orange,lemon,pear], function(err){
 //   if(err){
@@ -60,20 +97,56 @@ const pear = new Fruit({
 //   }
 // });
 
-Fruit.find(function(err, fruits){
-  if(err){
+// FIND & DISPLAY
+Fruit.find(function (err, fruits) {
+  if (err) {
     console.log(err);
-  }else{
+  } else {
 
     mongoose.connection.close();
-    
+
     // Arrays of objects to single/each object
-    fruits.forEach(function(fruit){
+    fruits.forEach(function (fruit) {
       console.log(fruit.name);
     });
   }
 });
 
+// UPDATE
+// Fruit.updateOne(
+//   {_id:"60d9f32cc1368f477c9a476f"},
+//   {name:"Rambutan"},
+//   function(err){
+//     if (err){
+//       console.log(err);
+//     }else{
+//       console.log("Successfully update!");
+//     }
+//   }
+// );
+
+// DELETE
+// Fruit.deleteOne(
+//   {name:"Lemon"},
+//   function(err){
+//     if(err){
+//       console.log(err);
+//     }else{
+//       console.log("Successfully delete!");
+//     }
+//   }
+// );
+// DELETE ALL
+// Fruit.deleteMany(
+//   {name:"John"},
+//   function(err){
+//     if(err){
+//       console.log(err);
+//     }else{
+//       console.log("Successfully delete all!");
+//     }
+//   }
+// );
 
 // find all through documents
 const findDocuments = function (db, callback) {
